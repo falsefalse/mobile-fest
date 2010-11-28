181e7d3fc64e24dbcaf0cbf8c37bd9fc5655a3a9
Ilya Furman <ifurman@voyageprive.com>
2010-11-28 15:39:54 +0200

    Reuse custom checkboxes on band page
    Generalizes styling.
    Improves JS:
     * bind checkbox creation to JQM pagecreate event
     * use classes for page and inputs selection, its more reliable and fast that attrs selectors
     * simplify checkbox toggle code
     * reoder js includes, our JQM dependent code should go before actual JQM, as was advices in API docs
     * do not use touchstart instead of clicking

--- js/main.js
+++ js/main.js
@@ -1,32 +1,35 @@
 // emulate checkboxes with styled spans
-$(function() {
-    $('input[data-beautify]').each(function(i, input) {
-        var $input = $(input);
-        var checked = $input.attr('checked');
-        $input.wrap(
-            $('<span>')
-                .addClass('ui-icon ui-icon-custom-checkbox ' + ((checked) ? 'ui-icon-custom-checkbox-on' : ''))
-        );
-        var $emulator = $input.parent();
+$(document).ready(function() {
+    // listen for page creation event
+    // bind listner to the page wrapper, so we will get only new inputs on each new page creation
+    // its important to listen to pagecreate, so checkboxes will be wrapped in div.ui-checkbox by the time we get in
+    $('div.whole-page').live('pagecreate', function(event) {
+        $(this)
+            .find('input.custom-checkbox')
+            .each(function(i, input) {
+                var $input = $(input);
+                var checked = $input.attr('checked');
 
-        $input.parents('.ui-checkbox').bind({
-            'touchstart mousedown' : function(event) {
-                // prevents whole row from being clicked
-                event.stopPropagation();
-            },
-            // touchstart is questionable here
-            // I am not sure about how checkbox clicks should be handled on iphone from UX point of view
-            'touchstart click' : function(event) {
-                if ($emulator.hasClass('ui-icon-custom-checkbox-on')) {
-                    $input.removeAttr('checked');
-                    $emulator.removeClass('ui-icon-custom-checkbox-on');
-                } else {
-                    $input.attr('checked', true);
-                    $emulator.addClass('ui-icon-custom-checkbox-on');
-                }
-                event.stopPropagation();
-                return false;
-            }
+                $input.wrap(
+                    $('<span>')
+                        .addClass('ui-icon ui-icon-custom-checkbox ' + ((checked) ? 'ui-icon-custom-checkbox-on' : ''))
+                );
+                // our fancy looking wrapper around the actual checkbox
+                var $emulator = $input.parent();
+
+                $input.parents('.ui-checkbox').bind({
+                    'touchstart mousedown' : function(event) {
+                        // prevents whole row from being clicked
+                        event.stopPropagation();
+                    },
+                    'click' : function(event) {
+                        var isOn = $emulator.hasClass('ui-icon-custom-checkbox-on');
+                        // !isOn makes checkbox and emulator actually change their state to opposite
+                        $input.attr('checked', !isOn);
+                        $emulator.toggleClass('ui-icon-custom-checkbox-on', !isOn);
+                        event.stopPropagation();
+                    }
+                });
         });
     });
 });
